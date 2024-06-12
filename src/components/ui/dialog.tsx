f2e77325b4da340cc506f4d2b2fe581a8d653b45
Raj kumar <glrajv369@gmail.com>
2024-06-12 10:06:22 +0530

    drawer custom component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -120,3 +120,66 @@ export {
   DialogTitle,
   DialogDescription,
 }
+
+type props = {
+  open?: boolean
+  closeModel?: () => void
+  title: string
+  description?: string
+  children: React.ReactNode
+  contentCls?: string
+  titleCls?: string
+  descriptionCls?: string
+  closeCls?: string
+}
+
+function DialogWrapper({
+  open = false, closeModel,
+  title, description = "", children,
+  contentCls = "", titleCls = "",
+  descriptionCls = "", closeCls = "",
+}: props) {
+  return (
+    <Dialog open={open} onOpenChange={closeModel}>
+      <DialogTrigger />
+
+      <DialogPortal>
+        <DialogOverlay />
+        <DialogPrimitive.Content
+          className={cn(
+            "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+            contentCls
+          )}
+        >
+          {
+            (title || description) &&
+            <DialogHeader>
+              {
+                title &&
+                <DialogTitle className={cn(titleCls)}>
+                  {title}
+                </DialogTitle>
+              }
+
+              {
+                description &&
+                <DialogDescription className={cn(descriptionCls)}>
+                  {description}
+                </DialogDescription>
+              }
+            </DialogHeader>
+          }
+
+          {children}
+
+          <DialogPrimitive.Close className={cn("absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground", closeCls)}>
+            <LuX className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </DialogPrimitive.Close>
+        </DialogPrimitive.Content>
+      </DialogPortal>
+    </Dialog>
+  )
+}
+
+export default DialogWrapper
