9109d8d8229f9d8dc3483e46a21c3f7485aa789d
RUBYNATHAN N <rubynathan999@gmail.com>
2024-06-29 14:12:02 +0530

    img upload

--- src/components/upload/index.tsx
+++ src/components/upload/index.tsx
@@ -1,55 +1,25 @@
 "use client";
 
+import axios from "axios";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
-import axios from "axios";
 
 function Upload() {
-  const [image, setImage] = useState<any>(null);
-
-  const uploadFile = async (timestamp: any, signature: any) => {
-    const data = new FormData();
-    data.append("file", image);
-    data.append("timestamp", timestamp);
-    data.append("signature", signature);
-    data.append("api_key", "168584872561438");
-    data.append("images", "images");
-
-    try {
-      const cloudName = "dz0vvjdhc";
-      const api = `https://api.cloudinary.com/v1_1/${cloudName}}/image/upload`;
-
-      const res = await axios.post(api, data);
-      const { secure_url } = res.data;
-      return secure_url;
-    } catch (error) {
-      console.error("Upload error:", error);
-    }
-  };
-
-  const getSignatureForUpload = async (folder: any) => {
-    try {
-      // const res = await axios.post(`${API}/sign-upload`, { folder });
-      // return res.data;
-    } catch (error) {
-      console.error("Signature error:", error);
-    }
-  };
+  const [image, setImage] = useState<any>();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
+    const formData = new FormData();
+    formData.append("image",image);
+
     try {
-      const { timestamp: imgTimestamp, signature: imgSignature }: any =
-        await getSignatureForUpload("images");
-      const imgUrl = await uploadFile(imgTimestamp, imgSignature);
-
-      // await axios.post(`${API}/api/videos`, { imgUrl});
-
-      console.log("File upload success!");
+      let res = await axios.put(`http://localhost:5000/users/imgupload`,formData);
+       console.log("img upload success",res?.data)
     } catch (error) {
-      console.error("Submit error:", error);
+      console.error("img upload error:", error);
     }
+    
   };
   return (
     <section className="min-h-screen flex justify-center items-center">
