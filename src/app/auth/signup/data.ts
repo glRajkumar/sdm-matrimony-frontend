17bae5ce4ba93906a4d6fc974b1a425f5a67f6d0
Raj kumar <glrajv369@gmail.com>
2025-03-08 15:30:54 +0530

    feilds change

--- src/app/auth/signup/data.ts
+++ src/app/auth/signup/data.ts
@@ -18,152 +18,247 @@ type SelectField = base & {
   }[];
 };
 
-type fields = BaseField | SelectField;
+export type fields = BaseField | SelectField;
 
-export const fieldList: fields[] = [
+type FieldListType = {
+  lable: string
+  list: fields[]
+};
+
+export const fieldList: FieldListType[] = [
   {
-    name: "fullName",
-    type: "text",
-    label: "Name",
-    rules: { required: "Name is required" },
-  },
-  {
-    name: "email",
-    type: "email",
-    rules: {
-      required: "Email is required",
-      pattern: {
-        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-        message: "Invalid email address"
-      }
-    },
-  },
-  {
-    name: "password",
-    type: "text",
-    rules: {
-      required: "Password is required",
-      minLength: {
-        value: 8,
-        message: "Password must be at least 8 characters"
-      }
-    },
-  },
-  {
-    name: "gender",
-    type: "select",
-    options: [
-      { label: "Male", value: "male" },
-      { label: "Female", value: "female" },
-      { label: "Other", value: "other" }
-    ],
-    rules: { required: "Gender is required" },
-  },
-  {
-    name: "dob",
-    type: "text",
-    label: "Date of Birth",
-    rules: { required: "Date of Birth is required" },
-  },
-  {
-    name: "qualification",
-    type: "text",
-    rules: { required: "Qualification is required" },
-  },
-  {
-    name: "work",
-    type: "text",
-    rules: { required: "Work is required" },
-  },
-  {
-    name: "salary",
-    type: "number",
-    label: "Salary per Month (in ₹)",
-    rules: {
-      required: "Salary is requird",
-      min: {
-        value: 0,
-        message: "Salary cannot be negative"
+    lable: "Account Details",
+    list: [
+      {
+        name: "email",
+        type: "email",
+        rules: {
+          required: "Email is required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Invalid email address"
+          }
+        },
       },
-      valueAsNumber: true
-    },
+      {
+        name: "password",
+        type: "text",
+        rules: {
+          required: "Password is required",
+          minLength: {
+            value: 8,
+            message: "Password must be at least 8 characters"
+          }
+        },
+      },
+    ]
   },
   {
-    name: "fatherName",
-    type: "text",
-    label: "Father's Name",
-    rules: { required: "Father's Name is required" },
+    lable: "Personal Details",
+    list: [
+      {
+        name: "fullName",
+        type: "text",
+        label: "Name",
+        rules: { required: "Name is required" },
+      },
+      {
+        name: "gender",
+        type: "select",
+        options: [
+          { label: "Male", value: "male" },
+          { label: "Female", value: "female" },
+          { label: "Other", value: "other" }
+        ],
+        rules: { required: "Gender is required" },
+      },
+      {
+        name: "dob",
+        type: "text",
+        label: "Date of Birth",
+        rules: { required: "Date of Birth is required" },
+      },
+      {
+        name: "height",
+        type: "number",
+        rules: { valueAsNumber: true },
+      },
+      {
+        name: "color",
+        type: "text",
+      },
+    ]
   },
   {
-    name: "motherName",
-    type: "text",
-    label: "Mother's Name",
-    rules: { required: "Mother's Name is required" },
+    lable: "Contact Details",
+    list: [
+      {
+        name: "mobile",
+        type: "number",
+      },
+      {
+        name: "address",
+        type: "text",
+      },
+    ]
   },
   {
-    name: "noOfBrothers",
-    type: "number",
-    label: "Number Of Brothers",
-    rules: { valueAsNumber: true },
+    lable: "Professional Details",
+    list: [
+      {
+        name: "qualification",
+        type: "text",
+        rules: { required: "Qualification is required" },
+      },
+      {
+        name: "work",
+        type: "text",
+        rules: { required: "Work is required" },
+      },
+      {
+        name: "salary",
+        type: "number",
+        label: "Salary per Month (in ₹)",
+        rules: {
+          required: "Salary is requird",
+          min: {
+            value: 0,
+            message: "Salary cannot be negative"
+          },
+          valueAsNumber: true
+        },
+      },
+    ]
   },
   {
-    name: "noOfSisters",
-    type: "number",
-    label: "Number Of Sisters",
-    rules: { valueAsNumber: true },
+    lable: "Family Details",
+    list: [
+      {
+        name: "fatherName",
+        type: "text",
+        label: "Father's Name",
+        rules: { required: "Father's Name is required" },
+      },
+      {
+        name: "motherName",
+        type: "text",
+        label: "Mother's Name",
+        rules: { required: "Mother's Name is required" },
+      },
+      {
+        name: "noOfBrothers",
+        type: "number",
+        label: "Number Of Brothers",
+        rules: { valueAsNumber: true },
+      },
+      {
+        name: "noOfSisters",
+        type: "number",
+        label: "Number Of Sisters",
+        rules: { valueAsNumber: true },
+      },
+      {
+        name: "birthOrder",
+        type: "number",
+        label: "Birth Order",
+        rules: { valueAsNumber: true },
+      },
+    ]
   },
   {
-    name: "birthOrder",
-    type: "number",
-    label: "Birth Order",
-    rules: { valueAsNumber: true },
+    lable: "Horoscope Details",
+    list: [
+      {
+        name: "nakshatra",
+        type: "text",
+      },
+      {
+        name: "rasi",
+        type: "text",
+      },
+      {
+        name: "lagna",
+        type: "text",
+      },
+      {
+        name: "dashaPeriod",
+        type: "text",
+        label: "Dasha Period",
+      },
+      {
+        name: "placeOfBirth",
+        type: "text",
+        label: "Place of Birth",
+      },
+      {
+        name: "timeOfBirth",
+        type: "text",
+        label: "Time of Birth",
+      },
+    ]
   },
   {
-    name: "placeOfBirth",
-    type: "text",
-    label: "Place of Birth",
+    lable: "Other Details",
+    list: [
+      {
+        name: "expectation",
+        type: "text",
+      },
+      {
+        name: "houseType",
+        type: "text",
+        label: "House Type",
+      },
+    ]
   },
   {
-    name: "nakshatra",
-    type: "text",
-  },
-  {
-    name: "rasi",
-    type: "text",
-  },
-  {
-    name: "lagna",
-    type: "text",
-  },
-  {
-    name: "expectation",
-    type: "text",
-  },
-  {
-    name: "formalities",
-    type: "text",
-  },
-  {
-    name: "houseType",
-    type: "text",
-    label: "House Type",
-  },
-  {
-    name: "address",
-    type: "text",
-  },
-  {
-    name: "dashaPeriod",
-    type: "text",
-    label: "Dasha Period",
-  },
-  {
-    name: "height",
-    type: "number",
-    rules: { valueAsNumber: true },
-  },
-  {
-    name: "color",
-    type: "text",
-  },
-];
+    lable: "Partner Preferences",
+    list: [
+      {
+        name: "minAge",
+        type: "number",
+        label: "Minimum Age",
+        rules: { valueAsNumber: true },
+      },
+      {
+        name: "maxAge",
+        type: "number",
+        label: "Maximum Age",
+        rules: { valueAsNumber: true },
+      },
+      {
+        name: "qualification",
+        type: "text",
+        label: "Qualification",
+      },
+      {
+        name: "work",
+        type: "text",
+        label: "Work",
+      },
+      {
+        name: "salary",
+        type: "number",
+        label: "Salary per Month (in ₹)",
+        rules: { valueAsNumber: true },
+      },
+      {
+        name: "religion",
+        type: "text",
+      },
+      {
+        name: "caste",
+        type: "text",
+      },
+      {
+        name: "motherTongue",
+        type: "text",
+        label: "Mother Tongue",
+      },
+      {
+        name: "location",
+        type: "text",
+      },
+    ]
+  }
+]
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,26 +1,17 @@
 "use client";
 
-import { Controller, useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { formatISO } from 'date-fns';
 import { Loader } from 'lucide-react';
 
 import { useSignup } from '@/hooks/use-account';
 import { fieldList } from './data';
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { DateTimePicker } from '@/components/ui/date-time-picker';
 import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
+import Fields from './fields';
 
 function Page() {
-  const { control, handleSubmit, register, formState: { errors } } = useForm<any>()
+  const methods = useForm()
 
   const { isPending, mutate } = useSignup()
 
@@ -41,108 +32,35 @@ function Page() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} id='signup-form'>
-      <div className='grid md:grid-cols-2 gap-4 -mr-8 pl-1 pr-8 pb-4 max-h-[50vh] overflow-y-auto'>
-        {
-          fieldList.map(field => {
-            if (field.type === "select") {
-              return (
-                <div key={field.name}>
-                  <Label
-                    htmlFor={`signup-${field.name}`}
-                    className='capitalize'
-                  >
-                    {field?.label || field?.name}
-                  </Label>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} id='signup-form'>
+        <div className='-mr-8 pl-1 pr-8 pb-4 max-h-[50vh] overflow-y-auto'>
+          {
+            fieldList.map(field => (
+              <div key={field.lable} className='mb-4'>
+                <h4 className="text-sm font-semibold text-gray-400">
+                  {field.lable}
+                </h4>
 
-                  <Controller
-                    name={field.name}
-                    control={control}
-                    rules={field?.rules || {}}
-                    render={({ field: { value, onChange } }) => (
-                      <Select value={value} onValueChange={onChange}>
-                        <SelectTrigger>
-                          <SelectValue placeholder="" />
-                        </SelectTrigger>
-
-                        <SelectContent>
-                          {
-                            field?.options?.map(op => (
-                              <SelectItem value={op.value} key={op.value}>
-                                {op.label}
-                              </SelectItem>
-                            ))
-                          }
-                        </SelectContent>
-                      </Select>
-                    )}
+                <div className='grid md:grid-cols-2 gap-4'>
+                  <Fields
+                    fields={field.list}
                   />
-                  {errors[field.name] &&
-                    // @ts-ignore
-                    <div className="text-xs text-red-500">{errors[field.name]?.message}</div>
-                  }
                 </div>
-              )
-            }
-
-            if (field?.name === "dob") {
-              return (
-                <div key={field.name}>
-                  <Label htmlFor="dob">Date of Birth</Label>
-                  <Controller
-                    name={field.name}
-                    control={control}
-                    rules={field?.rules || {}}
-                    render={({ field: { value, onChange } }) => (
-                      <DateTimePicker
-                        value={value}
-                        onChange={onChange}
-                        hourCycle={12}
-                      />
-                    )}
-                  />
-                  {errors[field.name] &&
-                    // @ts-ignore
-                    <div className="text-xs text-red-500">{errors[field.name]?.message}</div>
-                  }
-                </div>
-              )
-            }
-
-            return (
-              <div key={field.name}>
-                <Label
-                  htmlFor={`signup-${field.name}`}
-                  className='capitalize'
-                >
-                  {field?.label || field?.name}
-                </Label>
-
-                <Input
-                  id={`signup-${field.name}`}
-                  type={field.type}
-                  {...register(field.name, field?.rules || {})}
-                  className='no-number-arrows'
-                />
-
-                {errors[field.name] &&
-                  // @ts-ignore
-                  <div className="text-xs text-red-500">{errors[field.name]?.message}</div>
-                }
               </div>
-            )
-          })
-        }
-      </div>
+            ))
+          }
+        </div>
 
-      <Button
-        type="submit"
-        className="mt-6 w-full bg-pink-500 hover:bg-pink-600"
-      >
-        {isPending && <Loader className="animate-spin" />}
-        Sign Up
-      </Button>
-    </form>
+        <Button
+          type="submit"
+          className="mt-6 w-full bg-pink-500 hover:bg-pink-600"
+        >
+          {isPending && <Loader className="animate-spin" />}
+          Sign Up
+        </Button>
+      </form>
+    </FormProvider>
   )
 }
 
