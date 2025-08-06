5b35df43aea65e7b4d31bb0442927a232c295c4a
Raj kumar <glrajv369@gmail.com>
2025-08-06 20:10:55 +0530

    remove: utc format in calender

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -29,7 +29,6 @@ function Calendar({
 
   return (
     <DayPicker
-      timeZone="UTC"
       showOutsideDays={showOutsideDays}
       className={cn(
         "bg-background group/calendar p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
